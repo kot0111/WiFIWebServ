78c421a30de5012355fddfa7fecd8ce786528080
Kot0111 <34693879+kozyura0111@users.noreply.github.com>
2018-07-02 20:50:36 +1100

    Add code

---
+++ WiFiWebServerESP/WiFiWebServerESP.ino
@@ -0,0 +1,146 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+MDNSResponder mdns;
+String instring;
+
+// Wi-Fi
+const char* ssid = "SSID";
+const char* password = "Password";
+
+byte arduino_mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+IPAddress ip(192,168,1,35);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+ESP8266WebServer server(80);
+
+  int rel_act = 0;
+  int SERVO_ACT = 0;
+
+void setup(void){
+  // preparing GPIOs
+
+  delay(100);
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  WiFi.config(ip, gateway, subnet);
+
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  
+  if (mdns.begin("esp8266", WiFi.localIP())) {
+  }
+  
+  //+++++++++++++++++++++++ START  Relay-1 ++++++++++++++++++++
+  server.on("/", [](){
+    server.send(200, "text/html", webPage());
+  });
+  server.on("/socket1On", [](){
+    rel_act = 1;
+    Serial.println("turn1On");
+    server.send(200, "text/html", webPage());
+    delay(100);
+    
+  });
+  server.on("/socket1Off", [](){
+    rel_act = 0;
+ Serial.println("turn1Off");
+    server.send(200, "text/html", webPage());
+    delay(100);
+      });
+   //+++++++++++++++++++++++ END  Relay-1 ++++++++++++++++++++ 
+
+     //+++++++++++++++++++++++ START  temp-1 ++++++++++++++++++++
+
+server.on("/temp", [](){
+    Serial.println("tempUpd");
+    delay(200);
+    if (Serial.available() > 0){
+     instring = Serial.readStringUntil('\n');
+     }
+    server.send(200, "text/html", webPage());
+    delay(100);
+    
+  });
+     //+++++++++++++++++++++++ END  temp-1 ++++++++++++++++++++ 
+
+      //+++++++++++++++++++++++ START  Servo-1 ++++++++++++++++++++
+  server.on("/", [](){
+    server.send(200, "text/html", webPage());
+  });
+  server.on("/servo1On", [](){
+    SERVO_ACT = 1;
+    Serial.println("Servo180");
+    server.send(200, "text/html", webPage());
+    delay(100);
+    
+  });
+  server.on("/servo1Off", [](){
+    SERVO_ACT = 0;
+ Serial.println("Servo0");
+    server.send(200, "text/html", webPage());
+    delay(100);
+      });
+   //+++++++++++++++++++++++ END  Servo-1 ++++++++++++++++++++ 
+    
+    
+
+  server.begin();
+}
+ 
+void loop(void){
+  server.handleClient();
+} 
+
+String webPage()
+{
+  String web; 
+  web += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/> <meta charset=\"utf-8\"><title>ESP 8266</title><style>button{color:red;padding: 10px 27px;}</style></head>";
+  web += "<h1 style=\"text-align: center;font-family: Open sans;font-weight: 100;font-size: 20px;\">ESP8266 Web Server</h1><div>";
+  
+  //++++++++++ Relay-1  +++++++++++++
+  web += "<p style=\"text-align: center;margin-top: 0px;margin-bottom: 5px;\">----Relay----</p>";
+  if (rel_act == 1)
+  {
+    web += "<div style=\"text-align: center;width: 98px;color:white ;padding: 10px 30px;background-color: #43a209;margin: 0 auto;\">ON</div>";
+  }
+  else 
+  {
+    web += "<div style=\"text-align: center;width: 98px;color:white ;padding: 10px 30px;background-color: #ec1212;margin: 0 auto;\">OFF</div>";
+  }
+  web += "<div style=\"text-align: center;margin: 5px 0px;\"> <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></div>";
+  // ++++++++ Relay-1 +++++++++++++
+  
+  // ========Temp-1=============
+  web += "<div style=\"text-align:center;margin-top: 20px;\"><a href=\"temp\"><button style=\"width:158px;\">Temp: " +instring+ "</button></a></div>";
+  // ========Temp-1=============
+
+  //++++++++++ Servo-1  +++++++++++++
+  web += "<p style=\"text-align: center;margin-top: 0px;margin-bottom: 5px;\">----Lock----</p>";
+  if (SERVO_ACT == 1)
+  {
+    web += "<div style=\"text-align: center;width: 98px;color:white ;padding: 10px 30px;background-color: #43a209;margin: 0 auto;\">Open</div>";
+  }
+  else 
+  {
+    web += "<div style=\"text-align: center;width: 98px;color:white ;padding: 10px 30px;background-color: #ec1212;margin: 0 auto;\">Close</div>";
+  }
+  web += "<div style=\"text-align: center;margin: 5px 0px;\"> <a href=\"servo1On\"><button>Open</button></a>&nbsp;<a href=\"servo1Off\"><button>Close</button></a></div>";
+  // ++++++++ Servo-1 +++++++++++++
+ 
+  
+  // ========REFRESH=============
+  web += "<div style=\"text-align:center;margin-top: 20px;\"><a href=\"/\"><button style=\"width:158px;\">REFRESH</button></a></div>";
+  // ========REFRESH=============
+  
+  
+  web += "</div>";
+  return(web);
+}
---
+++ WiFiWebServerUno/WiFiWebServerUno.ino
@@ -0,0 +1,49 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <math.h>
+#define REL_PIN 12
+#define TEMP_PIN A3
+SoftwareSerial esp(4, 5);
+Servo myServo;
+void setup() {
+  esp.begin(9600);
+  pinMode(REL_PIN, OUTPUT);
+  pinMode(2, OUTPUT);
+  digitalWrite(REL_PIN, LOW);
+  myServo.attach(10);
+  digitalWrite(2,HIGH);
+  myServo.write(0);
+  delay(500);
+  digitalWrite(2,LOW);  
+
+}
+void loop() {
+
+  String command = esp.readStringUntil('\n');
+  command.trim();
+  if (command == "turn1On") {
+    
+    digitalWrite(REL_PIN, HIGH);
+  }
+  if (command == "turn1Off") {
+    digitalWrite(REL_PIN, LOW);
+  }
+  if (command == "tempUpd") {
+    float voltage = analogRead(TEMP_PIN) / 1024.0 * 5.0 - 0.5;
+    float temp = voltage * 100.0;
+    esp.println  (temp);
+  }
+  if (command == "Servo180") {
+      digitalWrite(2,HIGH);
+    myServo.write(180);
+      delay(500);
+      digitalWrite(2,LOW); 
+  }
+  if (command == "Servo0") {
+      digitalWrite(2,HIGH);
+    myServo.write(0);
+      delay(500);
+      digitalWrite(2,LOW); 
+  }
+}
